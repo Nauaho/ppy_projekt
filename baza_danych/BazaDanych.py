8a088669d5a813ce745be2d13078616e838f0b83
Nauaho <s25249@pjwstk.edu.pl>
2023-06-11 00:22:29 +0200

    Nowe funkcje + poprawa wypełnienia

--- baza_danych/BazaDanych.py
+++ baza_danych/BazaDanych.py
@@ -36,3 +36,24 @@ class BazaDanych:
             return None
         else:
             return user[0]
+        
+    def admin_o_takim_loginie(self, login):
+        sql = "SELECT * FROM Adminy WHERE login = ?;"
+        login = (login,)
+        cursor = self.bd.cursor()
+        cursor.execute(sql, login)
+        admin = cursor.fetchall()
+        if(len(admin) == 0):
+            return None
+        else:
+            return admin[0][0]
+        
+    def usun_zadanie(self,id):
+        sql = "DELETE FROM Zadania WHERE id = ?;"
+        id = (id,)
+        c = self.bd.execute(sql, id)
+        self.bd.commit()
+        if(c.rowcount == 0):
+            return "Zadania o takim identyfikatorze nie istnieje"
+        else:
+            return "Zadanie zostało usunięte"
--- baza_danych/wypelnienie.py
+++ baza_danych/wypelnienie.py
@@ -30,8 +30,8 @@ zadania = (
     ( None, 1, 1, 'nazar', 'ola', 'Backend', 'Zrób zwykły backend z wygodnym interfacem, albo zabiję',datetime.datetime.now(), datetime.datetime.now() + datetime.timedelta(days=1)),
     ( None, 2, 2, 'ola', 'admin', 'Frontend', 'Zrób zwykły frontend z wygodnym GUI :3 ',datetime.datetime.now(), datetime.datetime.now() + datetime.timedelta(weeks=1)),
     ( None, 3, 1, 'nazar', 'admin', 'Kup Plecak', 'Kup Plecak zrobiony ze złota',datetime.datetime.now(), datetime.datetime.now() + datetime.timedelta(days=2)),
-    ( None, 4, 3, 'nazar', None, 'Sprobować sushi', 'Chcę sushi',datetime.datetime.now(), datetime.datetime.now() + datetime.timedelta(hours=1)),
-    ( None, 5, 2, 'admin', None, 'Kupić nową koszulkę', 'Kupić fajną koszulkę z Pink Floyd',datetime.datetime.now(), datetime.datetime.now() + datetime.timedelta(weeks=4)))
+    ( None, 2, 3, 'nazar', None, 'Sprobować sushi', 'Chcę sushi',datetime.datetime.now(), datetime.datetime.now() + datetime.timedelta(hours=1)),
+    ( None, 3, 2, 'admin', None, 'Kupić nową koszulkę', 'Kupić fajną koszulkę z Pink Floyd',datetime.datetime.now(), datetime.datetime.now() + datetime.timedelta(weeks=4)))
 
 def seed(bd):
     for u in uzytkownicy:
